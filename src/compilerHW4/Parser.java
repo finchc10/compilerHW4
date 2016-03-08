e6df4732510fea8cc18ba6478c9710dcad73a884
Cory <Cory@Cory-PC.home>
2016-03-07 19:26:04 -0500

    grammar rules above each method

--- src/compilerHW4/Parser.java
+++ src/compilerHW4/Parser.java
@@ -10,107 +10,107 @@ public class Parser
 {
 	String input = "";
 	
-//	Program -> alpha VarDecl* Stm* omega
-//	VarDecl -> declare Type id
-//	Type -> abacus
-//	         -> tome
-//	         -> dichotomy
-//	Stm -> under_contract(Exp) Stmt* end_contract
-//	       -> consider(Exp) Stm* end_consider
-//	       -> print(Exp)
-//	       -> id Assign
-//	Assign -> peer Exp
-//	Exp -> And Elist
-//	Elist -> fus And Elist
-//	        -> null
-//	And -> Less Alist
-//	AList -> serfTo Less AList
-//	         -> null
-//	Less -> Term Llist
-//	Llist -> loot Term Llist
-//	        -> drop Term Llist
-//	        -> null
-//	Term -> Not TList
-//	TList -> buff Not TList
-//	          -> nerf Not TList
-//	         -> null
-//	Not -> dah Not 
-//	       -> Factor
-//	Factor -> number
-//	            -> aye
-//	            -> nay
-//	            -> id
-
+	//	Program -> alpha VarDecl* Stm* omega
 	public void Program()
 	{
 		
 	}
 	
+	//	VarDecl -> declare Type id
 	public void VarDecl()
 	{
 		
 	}
 	
+	//	Type -> abacus
+	//    -> tome
+	//    -> dichotomy
 	public void Type()
 	{
 		
 	}
 	
+	//	Stm -> under_contract(Exp) Stmt* end_contract
+	//    -> consider(Exp) Stm* end_consider
+	//    -> print(Exp)
+	//    -> id Assign
 	public void Stm()
 	{
 		
 	}
 	
+	//	Assign -> peer Exp
 	public void Assign()
 	{
 		
 	}
 	
+	//	Exp -> And Elist
 	public void Exp()
 	{
 		
 	}
 	
+	//	Elist -> fus And Elist
+	//    -> null
 	public void EList()
 	{
 		
 	}
 	
+	//	And -> Less Alist
 	public void And()
 	{
 		
 	}
 	
+	//	AList -> serfTo Less AList
+	//    -> null
 	public void AList()
 	{
 		
 	}
 	
+	//	Less -> Term Llist
 	public void Less()
 	{
 		
 	}
 	
+	//	Llist -> loot Term Llist
+	//    -> drop Term Llist
+	//    -> null
 	public void LList()
 	{
 		
 	}
 	
+	//	Term -> Not TList
 	public void Term()
 	{
 		
 	}
 	
+	//	TList -> buff Not TList
+	//    -> nerf Not TList
+	//   -> null
 	public void TList()
 	{
 		
 	}
 	
+	
+	//	Not -> dah Not 
+	//    -> Factor
 	public void Not()
 	{
 		
 	}
 	
+	//	Factor -> number
+	//    -> aye
+	//    -> nay
+	//    -> id
 	public void Factor()
 	{
 		
