c157ee336dc5c9601109be00d43d9ac4a37179f9
Cory <Cory@Cory-PC.home>
2016-03-07 19:23:05 -0500

    initial methods

--- src/compilerHW4/Parser.java
+++ src/compilerHW4/Parser.java
@@ -8,5 +8,116 @@ package compilerHW4;
 
 public class Parser
 {
+	String input = "";
+	
+//	Program -> alpha VarDecl* Stm* omega
+//	VarDecl -> declare Type id
+//	Type -> abacus
+//	         -> tome
+//	         -> dichotomy
+//	Stm -> under_contract(Exp) Stmt* end_contract
+//	       -> consider(Exp) Stm* end_consider
+//	       -> print(Exp)
+//	       -> id Assign
+//	Assign -> peer Exp
+//	Exp -> And Elist
+//	Elist -> fus And Elist
+//	        -> null
+//	And -> Less Alist
+//	AList -> serfTo Less AList
+//	         -> null
+//	Less -> Term Llist
+//	Llist -> loot Term Llist
+//	        -> drop Term Llist
+//	        -> null
+//	Term -> Not TList
+//	TList -> buff Not TList
+//	          -> nerf Not TList
+//	         -> null
+//	Not -> dah Not 
+//	       -> Factor
+//	Factor -> number
+//	            -> aye
+//	            -> nay
+//	            -> id
 
+	public void Program()
+	{
+		
+	}
+	
+	public void VarDecl()
+	{
+		
+	}
+	
+	public void Type()
+	{
+		
+	}
+	
+	public void Stm()
+	{
+		
+	}
+	
+	public void Assign()
+	{
+		
+	}
+	
+	public void Exp()
+	{
+		
+	}
+	
+	public void EList()
+	{
+		
+	}
+	
+	public void And()
+	{
+		
+	}
+	
+	public void AList()
+	{
+		
+	}
+	
+	public void Less()
+	{
+		
+	}
+	
+	public void LList()
+	{
+		
+	}
+	
+	public void Term()
+	{
+		
+	}
+	
+	public void TList()
+	{
+		
+	}
+	
+	public void Not()
+	{
+		
+	}
+	
+	public void Factor()
+	{
+		
+	}
+	
+	public void eat()
+	{
+		// dunno what eat does!
+	}
 }
